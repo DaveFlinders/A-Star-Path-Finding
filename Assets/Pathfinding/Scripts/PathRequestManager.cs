efe76f02a47af56acf77b407aaa03d5d37e72225
DaveFlinders <31257226+DaveFlinders@users.noreply.github.com>
2018-05-22 18:21:50 +0100

    Initial Push

---
+++ Assets/Pathfinding/Scripts/PathRequestManager.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+using System.Threading;
+
+public class PathRequestManager : MonoBehaviour {
+
+	Queue<PathResult> results = new Queue<PathResult>();
+
+	static PathRequestManager instance;
+	Pathfinding pathfinding;
+
+    public Queue<PathResult> Results
+    {
+        get
+        {
+            return results;
+        }
+    }
+
+    public static PathRequestManager Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = FindObjectOfType<PathRequestManager>();
+            }
+
+            return instance;
+        }
+    }
+
+    void Awake()
+    {
+		pathfinding = GetComponent<Pathfinding>();
+	}
+
+	void Update()
+    {
+		if (Results.Count > 0)
+        {
+			int itemsInQueue = Results.Count;
+			lock (Results)
+            {
+				for (int i = 0; i < itemsInQueue; i++)
+                {
+					PathResult result = Results.Dequeue ();
+					result.callback (result.path, result.success);
+				}
+			}
+		}
+	}
+
+	public static void RequestPath(PathRequest request)
+    {
+		ThreadStart threadStart = delegate 
+        {
+			Instance.pathfinding.FindPath (request, Instance.FinishedProcessingPath);
+		};
+
+		threadStart.Invoke ();
+	}
+
+	public void FinishedProcessingPath(PathResult result)
+    {
+		lock (Results)
+        {
+			Results.Enqueue (result);
+		}
+	}
+}
+
+public struct PathResult
+{
+	public Vector3[] path;
+	public bool success;
+	public Action<Vector3[], bool> callback;
+
+	public PathResult (Vector3[] path, bool success, Action<Vector3[], bool> callback)
+	{
+		this.path = path;
+		this.success = success;
+		this.callback = callback;
+	}
+
+}
+
+public struct PathRequest
+{
+	public Vector3 pathStart;
+	public Vector3 pathEnd;
+	public Action<Vector3[], bool> callback;
+
+	public PathRequest(Vector3 _start, Vector3 _end, Action<Vector3[], bool> _callback)
+    {
+		pathStart = _start;
+		pathEnd = _end;
+		callback = _callback;
+	}
+
+}
---
+++ Assets/test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+    Collider m_Collider;
+    Vector3 m_Center;
+    Vector3 m_Size, m_Min, m_Max;
+
+    void Start()
+    {
+        //Fetch the Collider from the GameObject
+        m_Collider = GetComponent<Collider>();
+
+    }
+
+    private void Update()
+    {
+        //transform.Translate(transform.right * Time.deltaTime);
+        //Grid.Instance.UpdatePartialGrid(m_Collider.bounds);
+    }
+
+}
